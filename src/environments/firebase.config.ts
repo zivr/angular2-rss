25f5a2290ee3cf84d241eec4f215afebcfa899fd
Shaul Naim <shaul@Shauls-MacBook-Pro.local>
2016-12-07 12:34:45 +0200

    add angularfire2

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA-HlcV4jtbhB0sL2D74SK9RVH9oZIQgVU",
+    authDomain: "js-fuseday-2016-stream.firebaseapp.com",
+    databaseURL: "https://js-fuseday-2016-stream.firebaseio.com",
+    storageBucket: "js-fuseday-2016-stream.appspot.com",
+    messagingSenderId: "533084422648"
+};
