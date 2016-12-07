17c230514586a35d8806709c970421c736de0534
Shaul Naim <shaul@Shauls-MacBook-Pro.local>
2016-12-07 14:34:44 +0200

    indexDB

--- src/app/feed-page/feed-page.component.ts
+++ src/app/feed-page/feed-page.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { FeedService } from '../feed.service';
 import { FeedEntry } from '../model/feed-entry';
-import {AngularFire,AngularFireModule, FirebaseListObservable} from 'angularfire2';
-import { firebaseConfig } from '../../environments/firebase.config';
 
 @Component({
   selector: 'app-feed-page',
   templateUrl: './feed-page.component.html',
   styleUrls: ['./feed-page.component.css']
 })
+
+
 export class FeedPageComponent implements OnInit {
 
   private feedUrl: string = 'https%3A%2F%2Fwww.becompany.ch%2Fen%2Fblog%2Ffeed.xml';
   private feeds: Array<FeedEntry> = [];
 
-   items: FirebaseListObservable<any[]>;
-  constructor(af: AngularFire) {
-    this.items = af.database.list('/stream');
-    this.items.subscribe(console.log);
-  }
+  constructor(
+    private feed: FeedService
+  ) { }
+
 
   ngOnInit() {
+    this.feed.getData(300);
   }
 
 
--- src/app/feed.service.ts
+++ src/app/feed.service.ts
@@ -2,33 +2,82 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 import { Feed } from './model/feed';
+import {AngularFire,AngularFireModule, FirebaseListObservable} from 'angularfire2';
+import { firebaseConfig } from '../environments/firebase.config';
+
 
 @Injectable()
 export class FeedService {
+  
+  readonly dbName = 'newsDB';
 
-  private rssToJsonServiceBaseUrl: string = 'https://rss2json.com/api.json?rss_url=';
-
-  constructor(
-    private http: Http
-  ) { }
-
-  getFeedContent(url: string): Observable<Feed> {
-    return this.http.get(this.rssToJsonServiceBaseUrl + url)
-            .map(this.extractFeeds)
-            .catch(this.handleError);
+  items: FirebaseListObservable<any[]>;
+  private IndexDb: IDBFactory; 
+  private db: IDBDatabase;
+  
+  constructor(af: AngularFire,private http: Http) {
+    this.items = af.database.list('/stream_2');
+    this.IndexDb = window.indexedDB;
+    this.createDB();
+    this.items.subscribe(this.onDataRecieved.bind(this));
+  }
+  
+  private createDB(){
+    var req: IDBOpenDBRequest;
+    req = this.IndexDb.open(this.dbName);
+    req.onupgradeneeded = (e: any) => {
+      var db = e.target.result;
+      db.createObjectStore('feeds', { keyPath: 'id'});
+    }
+  }
+  private updateDataInDB(data: Array<any>) {
+    var req: IDBOpenDBRequest;
+    req = this.IndexDb.open(this.dbName);
+    req.onsuccess = (e: any) => {
+      this.db = e.target.result;
+      var tbl: IDBObjectStore = this.db.transaction(['feeds'],'readwrite').objectStore('feeds');
+      data.forEach(obj => {
+        tbl.add(obj);
+      });
+    }
   }
 
-  private extractFeeds(res: Response): Feed {
-    let feed = res.json();
-    return feed || { };
+  private onDataRecieved(data: any) {
+    var myFeed: any = data.map((obj) => {
+      return {
+        id: obj.$key,
+        title: obj.title,
+        url: obj.url,
+        author: obj.author,
+        text: obj.text,
+        published: obj.published,
+        country: obj.thread.country,
+        site: obj.thread.site
+      }
+    });
+    this.updateDataInDB(myFeed);
   }
 
-  private handleError (error: any) {
-    // In a real world app, we might use a remote logging infrastructure
-    // We'd also dig deeper into the error to get a better message
-    let errMsg = (error.message) ? error.message :
-      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-    console.error(errMsg); // log to console instead
-    return Observable.throw(errMsg);
+  public getData(amount: number) {
+    return {};
   }
+  // getFeedContent(url: string): Observable<Feed> {
+  //   return this.http.get(this.rssToJsonServiceBaseUrl + url)
+  //           .map(this.extractFeeds)
+  //           .catch(this.handleError);
+  // }
+
+  // private extractFeeds(res: Response): Feed {
+  //   let feed = res.json();
+  //   return feed || { };
+  // }
+
+  // private handleError (error: any) {
+  //   // In a real world app, we might use a remote logging infrastructure
+  //   // We'd also dig deeper into the error to get a better message
+  //   let errMsg = (error.message) ? error.message :
+  //     error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+  //   console.error(errMsg); // log to console instead
+  //   return Observable.throw(errMsg);
+  // }
 }
